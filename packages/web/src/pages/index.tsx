6065a286fde513bb920d0e206b5c9570602ceb7e
chengtianhua <1057691751@qq.com>
2023-08-15 16:59:28 +0800

    调整部分样式

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,23 +2,24 @@ import { useEffect } from 'react';
 // @ts-ignore
 import Editor from '@sceditor/editor-core';
 import React from 'react';
-// import { PageContainer } from '@micro-frame/sc-runtime';
-// import { history } from 'umi';
-// export default function IndexPage() {
-//   document.domain = '192.168.2.193';
-//   useEffect(() => {
-//     console.log(window.location);
-//   }, []);
-//   return <Editor iframeUrl="http://192.168.31.17:10086/#/pages/index/index" />
-// }
 
-const Decorate: React.FC<any> = () => {
-  const { origin } = window.location;
+export default function IndexPage() {
+  document.domain = '192.168.31.182';
   useEffect(() => {
     console.log(window.location);
   }, []);
+  return (
+    <Editor iframeUrl="http://192.168.31.182:10086/#/pages/preview/index" />
+  );
+}
 
-  return <Editor iframeUrl={`${origin}/preview`} />;
-};
+// const Decorate: React.FC<any> = () => {
+//   const { origin } = window.location;
+//   useEffect(() => {
+//     console.log(window.location);
+//   }, []);
 
-export default Decorate;
+//   return <Editor iframeUrl={`${origin}/preview`} />;
+// };
+
+// export default Decorate;
