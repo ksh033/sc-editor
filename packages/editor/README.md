ede62d5bbbbf17253551a459186f20b5d6a99ddd
ksh033 <94456671@qq.com>
2023-10-25 17:22:50 +0800

    新增

---
+++ packages/editor/README.md
@@ -0,0 +1,6 @@
+ "sideEffects": [
+    "es/**/*.less",
+    "lib/**/*.less",
+    "es/index.js",
+    "es/schema/initTpl.js" 
+  ] 防止webpack treeshaking不加载组件，自动注册执行不了
--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -2,9 +2,5 @@
 export * from './components/index';
 export * from './interface';
 export * from './schema/index';
-const Version = "1.0.0";
-export { Version }
 import {Editor} from '@sceditor/editor-core'
-console.log("Version",Version)
-
 export {Editor}
